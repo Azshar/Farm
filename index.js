7d347838fad12a4c98eb8f9b8868cf7229989330
Azshar <kon-marikyan@yandex.ru>
2024-04-04 17:55:26 +0300

    - install wix navigation

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('HomeScreen', () => App);
+
+Navigation.setDefaultOptions({
+  topBar: {
+    visible: false,
+  },
+});
+
+Navigation.events().registerAppLaunchedListener(() => {
+  return Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'HomeScreen',
+            },
+          },
+        ],
+      },
+    },
+  });
+});
